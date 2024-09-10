5a68fbfb553574a906809a5042279e12af6f8085
1drive1HB <1drive1drive1@gmail.com>
2024-09-10 23:34:32 +0200

    Init2

--- JS/background.js
+++ JS/background.js
@@ -2,3 +2,20 @@ document.addEventListener("DOMContentLoaded", function () {
     const video = document.getElementById("background-video");
     video.play();
 });
+
+/**section for table link */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all rows within the table body
+    const rows = document.querySelectorAll('#movies-series-table tbody tr');
+
+    // Iterate over each row and add a click event listener
+    rows.forEach(row => {
+        row.addEventListener('click', function() {
+            const url = this.getAttribute('data-url');
+            if (url) {
+                window.location.href = url;
+            }
+        });
+    });
+});
